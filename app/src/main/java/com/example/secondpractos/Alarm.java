da2e1519cddffc2cea476b406553bf384ed92a1f
xizzin <xizzincol@gmail.com>
2024-10-27 18:19:35 +0300

    upgrade

--- app/src/main/java/com/example/secondpractos/Alarm.java
+++ app/src/main/java/com/example/secondpractos/Alarm.java
@@ -3,22 +3,34 @@ package com.example.secondpractos;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
 
 public class Alarm extends BroadcastReceiver {
     /*
         при окончании таймера вывод вибрации и сообщения
      */
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     public void onReceive(Context context, Intent intent){
-        Toast.makeText(context, "Время вышло!", Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "БУДИЛЬНИК!", Toast.LENGTH_LONG).show();
         Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
-
         /*
         vibrator.vibrate(500); is deprecated as of api26
          */
+        Uri path = Uri.parse("android.resource://" + context.getPackageName() + "/raw/price.mp3");
+        RingtoneManager.setActualDefaultRingtoneUri(
+                context.getApplicationContext(), RingtoneManager.TYPE_RINGTONE, path
+        );
+        Ringtone r = RingtoneManager.getRingtone(context.getApplicationContext(), path);
+        r.play();
     }
 }
--- app/src/main/java/com/example/secondpractos/MainActivity.java
+++ app/src/main/java/com/example/secondpractos/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.secondpractos;
 
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,23 +23,50 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editsec;
-    Button btnStartTimer;
+
+    Button btnStartAlarm;
+    Button btnEndAlarm;
+    TimePicker alarmTimePicker;
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
+
+    @SuppressLint("ShortAlarm")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        editsec = findViewById(R.id.EditSeconds);
-        btnStartTimer = findViewById(R.id.TimerStarter);
-        btnStartTimer.setOnClickListener(view -> {
-            int seconds = Integer.parseInt(editsec.getText().toString());
 
-            Intent intent = new Intent(MainActivity.this, Alarm.class);
+        alarmTimePicker = (TimePicker) findViewById(R.id.timePicker);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        btnStartAlarm = findViewById(R.id.TimerStarter);
+    }
 
-            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+    public void OnToggleClicked(View view) {
+        long time;
+        if (((ToggleButton) view).isChecked()) {
+            Toast.makeText(MainActivity.this, "будильник поставлен", Toast.LENGTH_SHORT).show();
 
-            alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + seconds * 1000L, PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE));
-        });
+        Calendar calendar = Calendar.getInstance();
+        Intent intent = new Intent(this, Alarm.class);
+        // we call broadcast using pendingIntent
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        // получаем из календаря настоящее время
+        calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getHour());
+        calendar.set(Calendar.MINUTE, alarmTimePicker.getMinute());
+
+        time = (calendar.getTimeInMillis() - (calendar.getTimeInMillis() % 60000));
+        if (System.currentTimeMillis() > time) {
+            // setting time as AM and PM
+            if (Calendar.AM_PM == 0)
+                time = time + (1000 * 60 * 60 * 12);
+            else
+                time = time + (1000 * 60 * 60 * 24); }
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 10000, pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+            Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_SHORT).show();
+        }
     }
 }
